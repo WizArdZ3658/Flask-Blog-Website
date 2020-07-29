4f47ecbe4893a4dc5f542b401d7a8bb823bbb0fa
Somnath <p.somnath2599@gmail.com>
2020-07-29 10:45:59 +0530

    first commit

---
+++ flask_app.py
@@ -0,0 +1,18 @@
+from flask import Flask, escape, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def hello():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
