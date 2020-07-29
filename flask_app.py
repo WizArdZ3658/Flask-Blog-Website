6bacdc15f89bea5143b8142c8e4400e22fc730ab
Somnath <p.somnath2599@gmail.com>
2020-07-29 19:37:09 +0530

    added pages for register/login

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,41 @@
-from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, render_template, flash, redirect, url_for
+from forms import LoginForm, RegistrationForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0936930773d65b75bf3be53fcf36d59f'
 
 
 @app.route('/')
 @app.route('/home')
-def hello():
+def home():
     return render_template('home.html')
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
